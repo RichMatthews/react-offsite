5f87fe2222e95c7b2a50d5ecab1ae5d4f6421220
Rich Matthews <richard@paddle.com>
2019-01-14 15:02:38 +0000

    working

--- src/data.js
+++
@@ -1,9 +0,0 @@
-export default {
-  countries: [
-    {name: 'Australia', code: 'au', visited: false},
-    {name: 'Afghanistan', code: 'af', visited: false},
-    {name: 'Bangladesh', code: 'bd', visited: false},
-    {name: 'Bahrain', code: 'bh', visited: false},
-    {name: 'Canada', code: 'ca', visited: false},
-  ]
-}
